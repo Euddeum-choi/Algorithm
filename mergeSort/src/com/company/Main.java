c704d0c7b99c51669a0e267e479ac4da6959f311
rmsdnjs518 <rmsdnjs518@gmail.com>
2022-10-21 21:12:50 +0900

    mergeSort complete

--- mergeSort/src/com/company/Main.java
+++ mergeSort/src/com/company/Main.java
@@ -21,4 +21,61 @@ public class Main {
             System.out.print(arr[i]+" ");
         }
     }
+
+    public static void mergeSort(int[] arr ){
+        sorted = new int[arr.length];
+        mergeSort(arr,0,arr.length-1);
+        sorted=null;
+    }
+
+    public static void mergeSort(int[] arr, int left, int right){
+        if (left == right){
+            return;
+        }
+        int mid = (left+right)/2;
+        mergeSort(arr,left,mid);
+        //왼쪽 쪼개는 재귀
+        mergeSort(arr,mid+1,right);
+        //오른쪽 쪼개는 재귀
+        mergerSort(arr, left,mid,right);
+        //합하는 -> override
+    }
+
+    public static void mergerSort(int[] arr, int left, int mid, int right){
+        int l = left;
+        int r = mid+1;
+        int idx = left;
+
+        while(l<=mid&&r<=right){
+            if(arr[l]<=arr[r]) {
+                sorted[idx] = arr[l];
+                idx++;
+                l++;
+            }
+            else{
+                sorted[idx] = arr[r];
+                idx++;
+                r++;
+            }
+        }
+
+        if(l>mid){
+            while(r<=right){
+                sorted[idx] = arr[r];
+                idx++;
+                r++;
+            }
+        }
+        else{
+            while(l<=mid){
+                sorted[idx] = arr[l];
+                idx++;
+                l++;
+            }
+        }
+
+        for(int i=left;i<=right;i++){
+            arr[i] = sorted[i];
+        }
+    }
 }
